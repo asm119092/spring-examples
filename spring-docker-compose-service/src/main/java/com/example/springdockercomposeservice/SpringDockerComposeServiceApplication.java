c8afaf249d8ad66a3c23ff15c82e17cb8205c131
ubuntu <ubuntu@ubuntu.myguest.virtualbox.org>
2024-01-03 02:45:55 +0530

    Spring docker compose feature

---
+++ spring-docker-compose-service/src/main/java/com/example/springdockercomposeservice/SpringDockerComposeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdockercomposeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerComposeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerComposeServiceApplication.class, args);
+	}
+
+}
---
+++ spring-docker-compose-service/src/test/java/com/example/springdockercomposeservice/SpringDockerComposeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdockercomposeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerComposeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
